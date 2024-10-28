f1edc45a58a9a1e613ffd66c6bafa5dd4f95a0c9
lukamerumovi <lukalukamerumeru@gmail.com>
2024-10-28 23:20:41 +0400

    second commit

---
+++ level 03/homework/homework.py
@@ -0,0 +1,26 @@
+#print ("goa best")
+
+#ცვლადი - variable
+
+#car = "მანქვინი "
+#age =  30
+
+
+#str string  - ბრჭყალებში მოქცეული ტექსტი
+#int intenger - მთელი რიცხვი
+#float - წილადი რიცხცვი
+#print(car + " არის "  +  str(age) + " წლის")
+
+num1 = 10
+num2 = 20
+
+
+print(num1 + " + " + num2 + " = " + str(int(num1) + int(num2)))
+
+
+
+
+#var1 = "hello"
+#var2 = "world" 
+
+#print(var1 +  " " + var2 )
