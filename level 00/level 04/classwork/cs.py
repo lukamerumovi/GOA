a149fca4df06fe455769d3eaaa7a83d236a615f8
lukamerumovi <lukalukamerumeru@gmail.com>
2024-11-07 18:01:19 +0400

    commit

---
+++ level 00/level 04/classwork/cs.py
@@ -0,0 +1,13 @@
+num1 = int(input("enter ur num: "))
+num2 = int(input("enter ur num: "))
+print(num1 + num2)
+
+num1 = int(input("enter ur num: "))
+num2 = int(input("enter ur num: "))
+
+
+print("+",num1 + num2)
+print("-",num1 - num2)
+print("*",num1 * num2)
+print("/",num1 / num2)
+
---
+++ level 00/level 05/classwork/s.py
@@ -0,0 +1,3 @@
+num = int(input("enter ur num"))
+num1 = int(input("enter ur num"))
+print(num + num1)
