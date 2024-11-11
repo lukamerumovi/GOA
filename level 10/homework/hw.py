f955ad337627c0db7ea70d89fe059932785c3302
lukamerumovi <lukalukamerumeru@gmail.com>
2024-11-11 22:50:07 +0400

    firth commit

---
+++ level 10/homework/hw.py
@@ -0,0 +1,11 @@
+num = int(input("please enter a number"))
+num1 = int(input("please enter second number"))
+
+if num or num1 == "+" :
+    print("addition")
+elif num or num1 == "-" :
+    print("deduction")
+elif num or num1 == "*" :
+    print("multiplication")
+elif num or num1 == "/" : 
+    print("division")
