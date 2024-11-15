09b7f2887a5f5b13edaa72456d0da3218aa33021
lukamerumovi <lukalukamerumeru@gmail.com>
2024-11-15 14:14:28 +0400

    commit

---
+++ level 12/homework/hm.py
@@ -0,0 +1,13 @@
+# for i in range(11):
+#     print(i)
+    
+# for i in range(100, 201):
+#     print(i)
+
+# for i in range(20, 50):
+#     if i %2 == 0:
+#         print(str(i) + "even")
+
+# for i in range(1, 49):
+#     if i % 2 != 0:
+#         print(str(i) , "odd")
