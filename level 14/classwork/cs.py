763dfd4928f7fc4bbbe9ba04a34972ada97b2fdc
lukamerumovi <lukalukamerumeru@gmail.com>
2024-11-16 15:12:16 +0400

    commit

---
+++ level 14/classwork/cs.py
@@ -0,0 +1,10 @@
+# დაბეჭდეთ 1 დან 20 მდე რიცხვების ჯამი
+
+# for i in range(0, 20):
+#     print(i)
+
+    #საშუალო არითმეტიკული
+# sum = 0
+# for i in range(1,50):
+#     sum = sum + i
+#     print(sum // 50)
