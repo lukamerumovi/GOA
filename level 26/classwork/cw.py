c4922705c01ea7490cb7aefca74aaa5e9664816e
lukamerumovi <lukalukamerumeru@gmail.com>
2024-11-25 23:51:29 +0400

    commit

---
+++ level 26/classwork/cw.py
@@ -0,0 +1,45 @@
+# my_list = ["nikolozi", "dato", "vitali", "gabi"] 
+
+# index = 0
+# for element in my_list:  
+#     if element == "vitali":
+#         print(index)
+#     index += 1
+
+
+
+# my_list = ["nikolozi", "dato", "vitali", "gabi"] 
+# my_list.insert(1, "lasha")
+# my_list.insert(4, "toko")
+# print(my_list)
+
+# print(my_list.index("dato"))
+
+# insert - nebismier indeqsze chavamatot nebismieri indeqsi 
+
+# extend - siis elementebis gagrdzeleba/gaertianeba
+
+# count - daxmarebit chven shegvidzia vnaxot ramdeni elementia ert siashi
+
+# number= [1,2,3,4]
+# number2=[5,6,7,8]
+# number.extend(number2)
+# print(number)
+
+#classwork
+#1
+# my_list = ["gio", "luka", "sandro", "vitali"]
+# my_list2=["lasha", "nika", "vaxo"]
+
+# my_list.extend(my_list2)
+# print(my_list)
+
+#2
+# my_list = ["gio", "luka", "sandro", "vitali", "nika",  "vaxo"]
+# my_list.insert(5, "sonik")
+# print(my_list)
+
+#3
+# my_list = ["gio", "luka", "sandro", "vitali", "nika",  "vaxo" "luka"]
+# print(my_list.count("luka"))
+
---
+++ level 26/homework/hw.py
@@ -0,0 +1,42 @@
+# my_list=[15, 15, 16, 16 ,16 ,16, 16, 16, 15, 17, 17, 17, 17, 19, 21, 21, 23, 25, 25, 26, 32]
+# print(my_list.count(15))
+# print(my_list.count(16))
+# print(my_list.count(17))
+# print(my_list.count(19))
+# print(my_list.count(21))
+# print(my_list.count(23))
+# print(my_list.count(25))
+# print(my_list.count(26))
+# print(my_list.count(32))
+
+
+# my_list=["luka", "gio", "lasha", "dito", "irakli", "luka", "mari", "lasha", "luka"]
+# print(my_list.count("luka"))
+
+#შექმენით სია რომელშიც იქნება რენდომ რიცხვები მოთავსებული შემდეგ კი შექმენით მეორე სია რომელშიც გადაიტანთ პირველი სიიდან მხოლოდ ლუწ რიცხვებს
+
+# my_list = [1,2,3,4,5,6,7,8,9]
+# my_list2 =[]
+# for i in my_list:
+#     if i %2 ==0:
+#         my_list2.append(i)
+# print(my_list2)
+
+        
+
+#  შექმენით სია რომელშიც იქნება მოთავსებული რენდომ რიცხვები შემდეგ კი ამ სიიდან ამოშალეთ ყველა ლუწი რიცხვი
+
+# my_list = [1,2,3,4,5,6,7,8,9]
+# for i in my_list:
+#     if i %2 == 0:
+#         my_list.remove(i)
+# print(my_list)
+
+
+# შექმენით სია რომელშიც იქნება სახელები შემდეგ კი შექმენით პროგრამა რომელიც ამოშლის ყველა სახელს რომელიც "t" ასოზე იწყება და ჩაამატებს ახალ სიაში
+
+# my_list = ["luka", "lasha", "toko", "giori", "nika"]
+# for i in my_list:
+#     if i[0] == "t":
+#         my_list.remove(i)
+# print(my_list)
