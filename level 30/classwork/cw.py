4f23de5dbb62c3b99158addb7641a4439e5c5a25
lukamerumovi <lukalukamerumeru@gmail.com>
2024-11-28 21:56:51 +0400

    commit

---
+++ level 30/classwork/cw.py
@@ -0,0 +1,48 @@
+# from turtle import *
+
+# def lokis():
+#     speed(2)
+#     for _ in range(4):
+#         forward(100)
+#         left(90)
+
+
+# for _ in range(4):
+#     lokis()
+#     penup()
+#     forward(150) 
+#     pendown()
+
+# exitonclick()
+
+
+
+#შექმენით რამდენიმე ფუნქცია სხვადასხვა მათემატიკური მოქმედებებისთვის ერთში, დაუმატეთ რიცხვები მეორეში გაამრავლეთ
+# num1 = 10
+# num2 = 20
+
+# def plus():
+#     print(num1 + num2)
+# def min():
+#     print(num1 - num2)
+# def gam():
+#     print(num1 * num2)
+# def num():
+#     print(num1 // num2)
+
+
+#     plus()
+#     min()
+#     num()
+#     sum()
+
+
+# შექმენით ფუნქცია რომელსაც არგუმენტად გადაეცემა რიცხვებით სავსე სია ამ ფუნქციამ კი უნდა დააბრუნოს ამ სიის რიცხვების ჯამი
+
+# def multiply (list):
+#     num = 0
+#     for i in list:
+#         num += i
+#     return num
+# print(multiply([1,2,3,4,5,6]))
+
---
+++ level 30/homework/hm.py
@@ -0,0 +1,67 @@
+#  მომხმარებელს შეეკითხეთ ორი რიცხვი შემდეგ კი შექმენით ფუნქცია რომელსაც არგუმენტად გადასცემთ ამ ორ რიცხვს ხოლო ფუნქცია დააბრუნებს ამ ორი რიცხვის ჯამს, ასევე გააკეთე ასეთი 4 ფუნქცია სხვადასხვა მათემატიკური მოქმედებებისთვის, გამოიყენეთ პარამეტრები და არგუმენტად გადაეცით მომხარებლის შემოტანილი რიცხვები
+
+
+# num1 = int(input("enter ur name :"))
+# num2 = int(input("enter ur name :"))
+# def sum (num1, num2):
+#     return num1 + num2 
+# print(sum(num1, num2))
+
+
+# num1 = int(input("enter ur name :"))
+# num2 = int(input("enter ur name :"))
+# def gas (num1, num2):
+#     return num1 - num2 
+# print(gas(num1, num2))
+
+
+# num1 = int(input("enter ur name :"))
+# num2 = int(input("enter ur name :"))
+# def lok (num1, num2):
+#     return num1 // num2 
+# print(lok(num1, num2))
+
+
+# num1 = int(input("enter ur name :"))
+# num2 = int(input("enter ur name :"))
+# def gom (num1, num2):
+#     return num1 * num2 
+# print(gom(num1, num2))
+
+
+# შექმენით ფუნქცია რომელსაც არგუმენტად გადაეცემა რიცხვებით სავსე სია ამ ფუნქციამ კი უნდა დააბრუნოს ამ სიის რიცხვების ჯამი
+# def multiply (list):
+#     num = 0
+#     for i in list:
+#         num += i
+#     return num
+# print(multiply([1,2,3,4,5,6,7,8,9]))
+
+
+#შექმენით ფუნქცია რომელსაც გადაეცემა არგუმენტად რიცხვი შემდეგ კი მან უნდა დაგვიბრუნოს ლუწია ეს რიცხვი თუ კენტი
+
+# num= int(input("enter ur num :"))
+# def lon(number):
+#     if number %2 == 0:
+#         return "even"
+#     else:
+#         return "odd"
+#     print(lon(num))
+
+
+#შექმენით ფუნქცია რომელსაც არგუმენტად გადაეცემა რიცხვი შემდეგ კი მან უნდა დაგვიბრუნოს ეს რიცხვი დადებითია თუ უარყოფითი
+
+# num = int(input("enter ur num :"))
+# def positive_or_negative (number):
+#     if number >= 0 :
+#         return "positive"
+#     else:
+#         return "negative"
+#     print(positive_or_negative(number))
+
+
+# შექმენით ფუნქცია რომელსაც არგუმენტად გადაეცემა რიცხვი შემდეგ კი ფუნქციამ უნდა დაგვიბრუნოს მისი საპირისპირო რიცხვი 
+# num = int(input("enter ur num :"))
+# def opposite(num):
+#     return num
+# print(num)
