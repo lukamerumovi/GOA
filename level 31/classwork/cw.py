7eb1aa3522076a194124571f57f81b5dfae19098
lukamerumovi <lukalukamerumeru@gmail.com>
2024-12-01 12:47:35 +0400

    commit

---
+++ level 31/classwork/cw.py
@@ -0,0 +1,25 @@
+# def multiply(a, b):
+#     return a * b
+
+# def even_or_odd(number):
+#     if number %2 == 0:
+#         return "Even"
+#     else:
+#         return "Odd"
+
+# def number_to_string(num):
+#     return str(num)
+
+# def opposite(number):
+#     return - number
+
+# def positive_sum(arr):
+#     sum = 0
+#     for i in arr:
+#         if i >0:
+#             sum += i
+#     return sum
+
+
+# def repeat_str(repeat, string):
+#     return repeat * string
---
+++ level 31/homework/hm.py
@@ -0,0 +1,43 @@
+# def basic_op(operator, value1, value2):
+#     if operator == "+":
+#         return value1 + value2
+#     elif operator == "-":
+#         return value1 - value2
+#     elif operator == "*":
+#         return value1 * value2
+#     elif operator == "/":
+#         return value1 / value2
+
+
+
+# def boolean_to_string(b):
+#     return str(b)
+
+
+# def string_to_number(s):
+#     return int(s)
+
+# def opposite(number):
+#     return - number 
+
+
+# def bool_to_word(boolean):
+#     if boolean == True:
+#         return "Yes"
+#     else:
+#         return "No"
+
+# def make_negative( number ):
+#     if number >0:
+#         return - number
+#     else:
+#         return number
+
+
+# def solution(string):
+#     return string[::-1]
+
+
+# def find_smallest_int(arr):
+#     return (min(arr))
+
