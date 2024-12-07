5e7717f8845e88c258a74bb9047252c65f2fbb41
lukamerumovi <lukalukamerumeru@gmail.com>
2024-12-07 14:15:57 +0400

    commit

---
+++ level 32/homework/hm.py
@@ -0,0 +1,9 @@
+# def count_sheeps(sheep):
+#     sum = 0
+#     for i in sheep:
+#         if i == True:
+#             sum += 1
+#     return sum
+
+
+
---
+++ level 35/homework/hm.py
@@ -0,0 +1,24 @@
+# def count_sheeps(sheep):
+#     sum = 0
+#     for i in sheep:
+#         if i == True:
+#             sum += 1
+#     return sum
+
+# def maps(a):
+#     return (a * 2)
+
+# def paperwork(n, m):
+#     if n <0 or m <0:
+#         return 0
+#     return n * m
+
+
+# def make_upper_case(s):
+#     return s.upper()
+
+# def simple_multiplication(number) :
+#     if number %2 == 0:
+#         return number * 8
+#     else:
+#         return number * 9
