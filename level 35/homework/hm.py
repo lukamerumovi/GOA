ce60c6aa9f2b608da823eb7f3ae579859d9530bd
lukamerumovi <lukalukamerumeru@gmail.com>
2024-12-11 03:09:20 +0400

    commit

--- level 35/homework/hm.py
+++ level 35/homework/hm.py
@@ -24,13 +24,8 @@
 #         return number * 9
 
 
-
-
-
-
-
-def are_you_playing_banjo(name):
-    if name[0] == 'R' or name[0] == 'r':
-        return name + " plays banjo"
-    else:
-        return name + " does not play banjo"
+# def maps(a):
+#     list = []
+#     for number in a:
+#         list.append(number * 2)
+#     return list
