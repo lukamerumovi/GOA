560c5fbca9f89d16925ad3832e11c604c3ffcbbc
lukamerumovi <lukalukamerumeru@gmail.com>
2024-12-12 18:07:21 +0400

    commit

---
+++ level 37/homework/hm.py
@@ -0,0 +1,33 @@
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# import random
+
+# list_of_group50 = ("ana puturidze", "atuka khuskivadze", "beka berashvili", "cotne gujabidze", "dachi alania", "dachi giorgadze", "giotgi tskhadadze", "goga chalauri", "mariam jalagonia", "saba bokuchava", "aleko gorjomelidze", "sofia gorgadze", "luka sexniashvili", "giga kochalidze", "vitali katsadze", "ucha khuberishvili")
+
+# group_members = []
+
+# names = []
+
+# while list_of_group50 != []:
+#     if len(list_of_group50) < 3 :
+#         break
+#     else:
+#         for i in range(4):
+#             random_students = random.choice(list_of_group50)
+#     names.append(random_students)
+#     list_of_group50.remove(random_students)
+#     group_members.append(names)
+#     names = []
+#     for element in group_members:
+#         print(element)
+#         print(names)
+
+
+# განმარტეთ RGB რას ნიშნავს და როგორ ვიყენებთ ფერების ასარჩევად
+
+# rgb იგივე რაც "red, green, blue საშუალებას გვაძლევს ამ ფუნქციის გამოყენების შემდეგ შევცვალოთ ფერები 0 დან 255 მდე, თუ ჩავწერთ მაქსიმუმს "red" ში, ანუ 255-ს შესაბამისად მას ექნება წითელი ფერი :> <3 "
+
+
+
+#კომენტარების სახით დაწერეთ რა განსხვავებაა id ,class და ელემენტის სელექტორს შორის და რომელია უფრო "ძლიერი"
+
+#id ელემენტი ეხება მხოლოდ ერთ ტექსტს, რომელშიცაა ჩაწერილი ხოლო "class" ელემენტს შეუძლია შეუცვალოს ფერი ერთა და მეტ ტექსტებს რომელშიც იქნება გამოყენებული
