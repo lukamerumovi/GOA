873c0d5754174391a19edd0cb9b588027caa5b6b
lukamerumovi <lukalukamerumeru@gmail.com>
2024-12-13 00:48:44 +0400

    commit

---
+++ level 34/classwork/cs.py
@@ -0,0 +1,18 @@
+# def summation(num):
+#     return sum(range(num + 1))
+
+# def century(year):
+#     return (year + 99) // 100
+
+# def no_space(x):    
+#     return x.replace (" ", "")
+
+# def litres(time):
+#     return int(time * 0.5)
+
+
+# def digitize(n):
+#     num = []
+#     for i in str(n):
+#         num.append(int(i))
+#     return(num[::-1])
