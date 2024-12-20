500273d780b6fe1dfcd8b112bf77a0c33698e743
lukamerumovi <lukalukamerumeru@gmail.com>
2024-12-20 04:03:36 +0400

    commit

---
+++ level 22/classwork/cw.py
@@ -0,0 +1,7 @@
+#მომხაარებელ შემოატანინეთ სახელი და გვარი ეს სახელი და გვარი გააერთიანეთ და შეაუბრუნეთ
+
+# num1 = input("enter ur name")
+# num2 = input("enter ur lastname")
+
+# bot = num1 + num2 
+# print(bot[::-1])
---
+++ level 22/homework/hm.py
@@ -0,0 +1,35 @@
+# list = []
+# list2 = ["ნიკა", "გიორგი", "საბა", "მარიამი", "ლაშა", "სოფო" , "დავითი"]
+
+# for i in list:
+#     if len(i) < 4:
+#         list.append(i)
+#         print(list)
+
+
+# num = [1,3,5,6,12,15,17,34,7,29,9]
+# new_list = []
+# for number in num:
+#     if number < 10:
+#         new_list.append(num)
+#     print(new_list)
+
+
+
+# text = ["bed", "sleep", "work"]
+# num = [1,2,3]
+# float = [2.2 , 3.4 , 5.6]
+
+# carieli_sia = []
+
+# carieli_sia.append(text)
+# carieli_sia.append(num)
+# carieli_sia.append(float)
+# print(carieli_sia)
+
+
+
+#კომენტარებით ახსენით რას შვება pop append
+
+# POP ფუნქცია გამოიყენება იმისათვის რომ ჩვენი სიიდან ამოვიღოთ რაიმე ელემენტი
+# APPEND ფუნქციის გამოყენებით კი ჩვენ შეგვიძლია სიაში დავამატოთ ახალი ელემენტი
