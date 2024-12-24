8e2261ce7ef70d3dad2245aa989a3776edd5710e
lukamerumovi <lukalukamerumeru@gmail.com>
2024-12-24 14:49:00 +0400

    commit

--- level 41/homework/hm.py
+++ level 41/homework/hm.py
@@ -1,1 +1,11 @@
-# ჩანაწერი არ დევს!
+# def filter_list(l):
+#   'return a new list with the strings filtered out'
+#   return [i for i in l if not isinstance(i, str)]
+
+
+# def to_jaden_case(string):
+#     return ' '.join(word.capitalize() for word in string.split())
+
+# def digital_root(n):
+#     return n if n < 10 else digital_root(sum(map(int,str(n))))
+
