5e30472703a3b430933de7d0c05c89d4995f6aaf
lukamerumovi <lukalukamerumeru@gmail.com>
2024-12-24 14:46:14 +0400

    'commit

--- level 41/classwork/ws.py
+++ level 41/classwork/ws.py
@@ -1,1 +1,45 @@
-# ჩანაწერი არ დევს!
+# def get_count(sentence):
+#     vowel = "aeiou"
+#     counter = 0
+#     for element in sentence:
+#         if element in lowel:
+#             counter += 1
+#             return counter
+
+
+# def square_digits(num):
+#     number = str(num)
+#     final_word = ""
+#     for element in number:
+#         final_word + str(int(element) * int(element))
+#         return int(final_word)
+
+
+
+# def high_and_low(numbers):
+#     my_list = numbers.split()
+#     high = int(my_list[0])
+#     low = int(my_list[0])
+#     for element in my_list:
+#         if int(element) > high:
+#             high = int(element)
+    
+#     for element in my_list:
+#         if int(element) < low:
+#             low = int(element)
+#     return str(high) + " " + str(low)
+
+
+
+
+# def get_middle(s):
+#     if len(s) %2 == 0:
+#         return s[(len(s)//2)-1] +s[len(s)//2]
+#     else:
+#         return s[len(s)//2]
+    
+
+
+
+
+
